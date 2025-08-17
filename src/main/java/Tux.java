8951a719a675fcb645f9db6ae039e9bc6a6f279b
fsythw <faithwansy123@gmail.com>
2025-08-17 21:31:46 +0800

    Implement skeleton

--- src/main/java/Tux.java
+++ src/main/java/Tux.java
@@ -1,10 +1,31 @@
-public class Duke {
+public class Tux {
+
+    private static final String NAME = "Tux";
+    private static final String DIVIDER = "____________________________________________________________";
+
+    private static String greetUser() {
+        return "Hello! I'm %s\nWhat can I do for you?".formatted(NAME);
+    }
+
+    private static String exit() {
+        return "Bye. Hope to see you again soon!";
+    }
+
+    private static void formatMessage(String message) {
+        System.out.println(DIVIDER);
+        System.out.println(message);
+
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        String name = "Tux";
+//        System.out.println("Hello from " + name);
+        formatMessage(greetUser());
+        formatMessage(exit());
+        System.out.println(DIVIDER);
     }
 }
