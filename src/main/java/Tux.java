f13b0d027d14a616ae6e2a36374b79508ad5e829
fsythw <faithwansy123@gmail.com>
2025-08-17 21:53:11 +0800

    Add item to list

--- src/main/java/Tux.java
+++ src/main/java/Tux.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.lang.StringBuilder;
 
 public class Tux {
 
     private static final String NAME = "Tux";
     private static final String DIVIDER = "____________________________________________________________";
     private static final String EXIT = "bye";
+    private static final String TASKLIST = "list";
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final List<String> taskList = new ArrayList<>();
+    private static final StringBuilder sb = new StringBuilder();
 
     private static String greetUser() {
         return "Hello! I'm %s\nWhat can I do for you?".formatted(NAME);
@@ -22,14 +28,31 @@ public class Tux {
         System.out.println(DIVIDER);
     }
 
+    private static void addToTaskList(String userInput) {
+        Tux.taskList.add(userInput);
+        formatMessage("added: %s".formatted(userInput));
+    }
+
+    private static void enumerateTaskList() {
+        for (int i = 0; i < Tux.taskList.size(); i++) {
+            //formatMessage("%s. %s".formatted(i+1, taskList.get(i)));
+            Tux.sb.append("%s. %s\n".formatted(i+1, taskList.get(i)));
+        }
+        formatMessage(sb.toString());
+    }
+
+
     private static void handleUserInput() {
         while (true) {
             String userInput = scanner.nextLine();
 
             if (userInput.equals(EXIT)) {
                 break;
+            } else if (userInput.equals(TASKLIST)){
+                enumerateTaskList();
             } else {
-                formatMessage(userInput);
+                addToTaskList(userInput);
+
             }
         }
     }
