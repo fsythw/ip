c4988f82414c9bae8da16f2bd5a27538eb011f5d
fsythw <faithwansy123@gmail.com>
2025-08-27 12:38:20 +0800

    Add JavaDoc comments

--- src/main/java/tux/tasks/TaskList.java
+++ src/main/java/tux/tasks/TaskList.java
@@ -9,10 +9,17 @@ import java.util.List;
 public class TaskList {
     private final List<Task> tasks;
 
+    /**
+     * Creates a TaskList object with an empty list.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Alternative constructor for TaskList with a list of tasks.
+     * @param initial List of tasks to be added.
+     */
     public TaskList(List<Task> initial) {
         this.tasks = new ArrayList<>(initial);
     }
@@ -25,9 +32,8 @@ public class TaskList {
         tasks.add(t);
     }
     public Task delete(int index) {
-        return tasks.remove(index - 1);
+        return tasks.remove(index);
     }
-
 
     public int size() {
         return tasks.size();
@@ -35,14 +41,6 @@ public class TaskList {
 
     public List<Task> asList() {
         return tasks;
-    }
-
-    public void clear() {
-        this.tasks.clear();
-    }
-
-    public void addAll(List<Task> tasks) {
-        this.tasks.addAll(tasks);
     }
 
     public Task get(int index) {
