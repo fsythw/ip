c1131d3c9a7c4a959d42818c4d75f2f9c15f7cdf
fsythw <faithwansy123@gmail.com>
2025-09-17 21:56:42 +0800

    Fix dummy test

--- src/test/java/tux/tasks/ToDoTest.java
+++ src/test/java/tux/tasks/ToDoTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 
 public class ToDoTest {
     @Test
-    public void dummyTest() {
-        assertEquals(2, 2);
+    public void createToDo_validInput_success() {
+        ToDo temporaryToDo = new ToDo("buy bread");
+        assertEquals("[T][ ] buy bread", temporaryToDo.getTaskDescription());
     }
 }
